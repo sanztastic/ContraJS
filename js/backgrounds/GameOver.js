f02fdeed70dadfe001e147240496af978d96edc2
Shanand Shrestha <51695785+sanztastic@users.noreply.github.com>
2021-02-04 08:20:03 +0545

    some font issues

--- js/backgrounds/GameOver.js
+++ js/backgrounds/GameOver.js
@@ -10,15 +10,15 @@ export default class GameOver {
         }
         ctx.fillStyle = "#000000";
         ctx.fillRect(0, 0, 512, 480);
-        ctx.font = '25px Contra NES';
+        ctx.font = '15px Contra NES';
         ctx.fillStyle = "#bcbcbc";
-        ctx.fillText(`PLAYER SCORE: ${player.score}`, 50, 50);
+        ctx.fillText(`PLAYER SCORE: ${player.score}`, 20, 40);
         // ctx.strokeText("Player Score: ", 10500, 100);
-        ctx.fillText(`HIGH SCORE: ${this.highScore}`, 50, 100);
+        ctx.fillText(`HIGH SCORE: ${this.highScore}`, 20, 60);
         // ctx.strokeText("High Score: ", 100, 100);
 
-        ctx.font = '40px Contra NES';
-        ctx.fillText(`GAME OVER`, 200, 256);
-        ctx.fillText('PRESS SPACE TO CONTINUE', 100, 350);
+        ctx.font = '18px Contra NES';
+        ctx.fillText(`GAME OVER`, 170, 200);
+        ctx.fillText('PRESS SPACE TO CONTINUE', 50, 280);
     }
 }
